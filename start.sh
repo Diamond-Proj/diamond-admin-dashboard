0e6e52bf432fa40e445627e24e7c549a6f20b76e
rohan-uiuc <rohan13@illinois.edu>
2024-12-13 10:36:14 -0600

    Remove backend

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Start Flask backend with Gunicorn
-gunicorn --workers=1 --threads=100 --worker-class=gthread api.backend.index:app --bind=0.0.0.0:${PORT:-5328} &
-
-# Start Next.js frontend in production mode
-PORT=${NEXT_PORT:-3000} node .next/standalone/server.js 
