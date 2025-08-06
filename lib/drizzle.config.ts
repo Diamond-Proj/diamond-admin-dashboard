41b7d7ccaa84fc3e3c45a056882523e649515d71
Minu Mathew <minumpmathews@gmail.com>
2025-08-06 16:28:52 -0500

    created a testdb instance and populated the current schema with drizzle

--- lib/drizzle.config.ts
+++ lib/drizzle.config.ts
@@ -6,8 +6,8 @@ export default defineConfig({
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost:5432/postgres',
+    url: process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost:5432/postgres',
   },
   verbose: true,
   strict: true
-} as any); 
+}); 
