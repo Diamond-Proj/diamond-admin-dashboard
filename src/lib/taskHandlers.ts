e450e6f2eb75c1c356e0b721cc460b0bc88ed832
Merrick Cai <merrickcai.dev@gmail.com>
2025-10-15 18:32:30 -0400

    chore: remove unused files, directories, dependencies, and functions for better project organization

--- src/lib/taskHandlers.ts
+++ src/lib/taskHandlers.ts
@@ -1,14 +1,15 @@
-import { toast } from '@/components/ui/use-toast';
-
 export async function getUserProfile(data: { identity_id: string }) {
   try {
     const { identity_id } = data;
-    const response = await fetch(`/api/profile?identity_id=${encodeURIComponent(identity_id)}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
+    const response = await fetch(
+      `/api/profile?identity_id=${encodeURIComponent(identity_id)}`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
       }
-    });
+    );
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
@@ -42,63 +43,6 @@ export async function updateUserProfile(data: any) {
   }
 }
 
-
-export async function submitTask(data: any) {
-  try {
-    const response = await fetch(`/api/submit_task`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    });
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    const responseData = await response.json();
-    console.log('submitTask responseData:', responseData);
-    return responseData;
-  } catch (error) {
-    console.error('Error in task submitting:', error);
-    throw error;
-  }
-}
-
-
-export async function registerContainer(data: {
-  endpoint: string;
-  base_image: string;
-  location: string;
-  name: string;
-  description: string;
-}): Promise<any> {
-  try {
-    const response = await fetch('/api/register_container', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data),
-      credentials: 'include'
-    });
-    if (!response.ok) {
-      console.log('response:', response);
-      throw new Error('Network response was not ok');
-    }
-    const responseData = await response.json();
-    console.log('register container response: ', responseData);
-    toast({
-      title: 'Success',
-      description: responseData.message
-    });
-    return responseData;
-  } catch (error) {
-    console.error('Error in registerContainer:', error);
-    throw error;
-  }
-}
-
-
 export async function registerAllEndpoints() {
   try {
     const response = await fetch('/api/register_all_endpoints', {
@@ -118,7 +62,6 @@ export async function registerAllEndpoints() {
     throw error;
   }
 }
-
 
 export async function loadAccountsPartitions(data: { endpoint_uuid: string }) {
   try {
@@ -140,4 +83,3 @@ export async function loadAccountsPartitions(data: { endpoint_uuid: string }) {
     throw error;
   }
 }
-
